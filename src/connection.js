2a1b93b4f9b3931bbe148e07ed0eaf1ef6eed3af
Akash Nair <akash.nair@sparwelt.de>
2019-11-06 16:52:20 +0100

    Added ENV config for MongoDB port

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,7 @@
 // connection.js
 const mongoose = require("mongoose");
-const dbUrl = "mongodb://mongo:27017/mongo-test";
+const DB_PORT = process.env.DB_PORT
+const dbUrl = `mongodb://mongo:${DB_PORT}/mongo-test`;
 const dbConnection = () => {
  return mongoose.connect(dbUrl, {useNewUrlParser:true});
 };
