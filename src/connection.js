3cfa78bdeaf129780e22f581dd8c7c28ddd3e814
Akash Nair <akash.nair@sparwelt.de>
2019-11-06 16:21:37 +0100

    Added kube manifests and update nodejs connection string

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 // connection.js
 const mongoose = require("mongoose");
-const dbUrl = "mongodb://localhost:27017/mongo-test";
+const dbUrl = "mongodb://mongo:27017/mongo-test";
 const dbConnection = () => {
  return mongoose.connect(dbUrl, {useNewUrlParser:true});
 };
